f5aa878eb37477ebce1cb7e7d5bf6d9e01ba50f6
Drevets <drevets@gmail.com>
2020-02-19 10:23:52 -0600

    adds bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './App.css';
 import Board from './Board.js'
 
-const App = () => {
+const Boards = () => {
   const [lists, setLists] = React.useState([])
   return (
+    <div className="container-fluid">
     <Board lists={lists} setLists={setLists}/>
+    </div>
   );
 }
 
-export default App;
+export default Boards;
